65f93dc0f71e531268833aed6e9117b97df9bc80
Pamol <pamol.waykar@gmail.com>
2025-07-15 12:51:14 +0100

    Completed the pamol-checking-account

--- pamol-checking-accounts/programs/pamol-checking-accounts/src/lib.rs
+++ pamol-checking-accounts/programs/pamol-checking-accounts/src/lib.rs
@@ -6,11 +6,26 @@ declare_id!("CAqotcekwXCRq2iWSDC6pSAoNHzhc7ptujM9MNQQofgT");
 pub mod pamol_checking_accounts {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn check_accounts(ctx: Context<CheckAccounts>) -> Result<()> {
+        msg!("Checking accounts for : {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CheckAccounts<'info> {
+    //check account is the signer
+    payer: Signer<'info>,
+
+    //Creating an account with no checking 
+    #[account(mut)]
+    account_to_create: UncheckedAccount<'info>,
+
+    //Creating an account with owner check 
+    #[account(mut, owner = id())]
+    account_to_change: UncheckedAccount<'info>,
+
+    //Check account is the executable and is the system program
+    system_program: Program<'info, System>,
+
+}
--- pamol-checking-accounts/tests/pamol-checking-accounts.ts
+++ pamol-checking-accounts/tests/pamol-checking-accounts.ts
@@ -1,16 +1,45 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { PamolCheckingAccounts } from "../target/types/pamol_checking_accounts";
+import { Keypair, SystemProgram, Transaction , sendAndConfirmTransaction} from "@solana/web3.js";
 
 describe("pamol-checking-accounts", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const wallet = provider.wallet as anchor.Wallet;
   const program = anchor.workspace.pamolCheckingAccounts as Program<PamolCheckingAccounts>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  //We will create it ahead of time so that our program will try to modify it
+  const accountToChange = new Keypair();
+  //Our program will create this
+  const accountToCreate = new Keypair();
+
+
+  it("Create an account owned by our program", async () => {
+    //Creating instruction to create an account
+    const instruction = SystemProgram.createAccount({
+      fromPubkey: provider.wallet.publicKey,
+      newAccountPubkey: accountToChange.publicKey,
+      lamports: await provider.connection.getMinimumBalanceForRentExemption(0),
+      space:0,
+      programId: program.programId, //Our program
+    });
+    //Creating Transanction by adding all the created instructions
+    const transaction = new Transaction().add(instruction);
+    //Send to the Transanction to blockchain to process and confirm
+    await sendAndConfirmTransaction(provider.connection, transaction, [wallet.payer, accountToChange]);
   });
+  it("Check accounts", async () => {
+    await program.methods
+      .checkAccounts()
+      .accounts({
+        payer:wallet.publicKey,
+        accountToCreate: accountToCreate.publicKey,
+        accountToChange: accountToChange.publicKey,
+      })
+      .rpc();
+  });
+
+
 });
